597e7da80b6c243a6f2f55da4c98cf2649d6a701
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-06 13:36:14 +0900

    Replace Application Gateway with Traffic Manager and VM public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,14 +49,7 @@ resource "azurerm_subnet" "primary_app" {
   address_prefixes     = ["10.10.10.0/24"]
 }
 
-resource "azurerm_subnet" "primary_agw" {
-  name                 = "snet-agw"
-  resource_group_name  = azurerm_resource_group.primary.name
-  virtual_network_name = azurerm_virtual_network.primary.name
-  address_prefixes     = ["10.10.1.0/24"]
-}
-
-# DR VNet
+# DR VNet skeleton. AP VM은 ASR Failover 후 Japan East 쪽에 생성되는 것을 가정합니다.
 resource "azurerm_virtual_network" "dr" {
   name                = "vnet-dr-app-jpe"
   location            = azurerm_resource_group.dr.location
@@ -71,11 +64,45 @@ resource "azurerm_subnet" "dr_app" {
   address_prefixes     = ["10.20.10.0/24"]
 }
 
-resource "azurerm_subnet" "dr_agw" {
-  name                 = "snet-agw"
-  resource_group_name  = azurerm_resource_group.dr.name
-  virtual_network_name = azurerm_virtual_network.dr.name
-  address_prefixes     = ["10.20.1.0/24"]
+# Public IP for Primary AP VM. Traffic Manager Azure endpoint requires a public endpoint.
+resource "azurerm_public_ip" "ap_pip" {
+  name                = "pip-prd-ap-tomcat01"
+  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.primary.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "${var.prefix}-prd-ap-krc"
+}
+
+# NSG for SSH and Tomcat test. 운영에서는 source_address_prefix를 본인 공인IP/32로 제한하세요.
+resource "azurerm_network_security_group" "app" {
+  name                = "nsg-prd-app-krc"
+  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.primary.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.allowed_admin_cidr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Tomcat-8080"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = var.allowed_admin_cidr
+    destination_address_prefix = "*"
+  }
 }
 
 # Primary AP VM NIC
@@ -89,14 +116,20 @@ resource "azurerm_network_interface" "ap" {
     subnet_id                     = azurerm_subnet.primary_app.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "10.10.10.10"
+    public_ip_address_id          = azurerm_public_ip.ap_pip.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "ap" {
+  network_interface_id      = azurerm_network_interface.ap.id
+  network_security_group_id = azurerm_network_security_group.app.id
 }
 
 resource "azurerm_linux_virtual_machine" "ap" {
   name                = "vm-prd-ap-tomcat01"
   location            = azurerm_resource_group.primary.location
   resource_group_name = azurerm_resource_group.primary.name
-  size                = "Standard_B2s"
+  size                = var.vm_size
   admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.ap.id
@@ -139,6 +172,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   sku_name               = "B_Standard_B1ms"
   version                = "8.0.21"
   backup_retention_days  = 7
+  zone                   = "1"
 
   # 운영에서는 DR 정책에 맞춰 geo-redundant backup 또는 replica/restore 전략 검토 필요
   geo_redundant_backup_enabled = true
@@ -152,69 +186,39 @@ resource "azurerm_mysql_flexible_database" "appdb" {
   collation           = "utf8mb4_unicode_ci"
 }
 
-# Application Gateway skeleton. 실제 운영에서는 HTTPS Listener, 인증서, WAF_v2, Probe 세부 설정 필요.
-resource "azurerm_public_ip" "agw_pip" {
-  name                = "pip-agw-prd-krc"
-  location            = azurerm_resource_group.primary.location
-  resource_group_name = azurerm_resource_group.primary.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# Traffic Manager: Application Gateway 정책 차단 환경에서 사용할 DNS 기반 DR Failover 예제
+resource "azurerm_traffic_manager_profile" "tm" {
+  name                   = "tm-${var.prefix}-tomcat-dr"
+  resource_group_name    = azurerm_resource_group.primary.name
+  traffic_routing_method = "Priority"
+
+  dns_config {
+    relative_name = "tm-${var.prefix}-tomcat-dr"
+    ttl           = 30
+  }
+
+  monitor_config {
+    protocol = "HTTP"
+    port     = 8080
+    path     = "/tomcat-test/index.jsp"
+  }
 }
 
-resource "azurerm_application_gateway" "agw" {
-  name                = "agw-prd-app-krc"
-  location            = azurerm_resource_group.primary.location
-  resource_group_name = azurerm_resource_group.primary.name
+resource "azurerm_traffic_manager_azure_endpoint" "primary" {
+  name               = "primary-korea-tomcat"
+  profile_id         = azurerm_traffic_manager_profile.tm.id
+  target_resource_id = azurerm_public_ip.ap_pip.id
+  priority           = 1
+}
 
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1
-  }
-
-  gateway_ip_configuration {
-    name      = "gw-ip-config"
-    subnet_id = azurerm_subnet.primary_agw.id
-  }
-
-  frontend_port {
-    name = "port-80"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "frontend-public"
-    public_ip_address_id = azurerm_public_ip.agw_pip.id
-  }
-
-  backend_address_pool {
-    name         = "prd-ap-pool"
-    ip_addresses = ["10.10.10.10"]
-  }
-
-  backend_http_settings {
-    name                  = "tomcat-http-8080"
-    cookie_based_affinity = "Disabled"
-    port                  = 8080
-    protocol              = "Http"
-    request_timeout       = 30
-  }
-
-  http_listener {
-    name                           = "listener-http"
-    frontend_ip_configuration_name = "frontend-public"
-    frontend_port_name             = "port-80"
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = "rule-tomcat"
-    rule_type                  = "Basic"
-    http_listener_name         = "listener-http"
-    backend_address_pool_name  = "prd-ap-pool"
-    backend_http_settings_name = "tomcat-http-8080"
-    priority                   = 100
-  }
+# DR endpoint는 ASR Test Failover 후 생성된 Japan East Public IP 또는 DNS로 교체합니다.
+# 초기에는 비활성화 상태로 생성합니다.
+resource "azurerm_traffic_manager_external_endpoint" "dr" {
+  name       = "dr-japan-tomcat"
+  profile_id = azurerm_traffic_manager_profile.tm.id
+  target     = var.dr_endpoint_fqdn
+  priority   = 2
+  enabled    = false
 }
 
 resource "azurerm_automation_account" "aa" {
@@ -233,4 +237,16 @@ resource "azurerm_recovery_services_vault" "asr" {
   location            = azurerm_resource_group.mgmt.location
   resource_group_name = azurerm_resource_group.mgmt.name
   sku                 = "Standard"
+}
+
+output "primary_vm_public_ip" {
+  value = azurerm_public_ip.ap_pip.ip_address
+}
+
+output "primary_vm_fqdn" {
+  value = azurerm_public_ip.ap_pip.fqdn
+}
+
+output "traffic_manager_fqdn" {
+  value = azurerm_traffic_manager_profile.tm.fqdn
 }
